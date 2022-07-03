cfff632a1d0a22a992f5bb027cc4a12320a315d4
Artem <uni744@yandex.ru>
2022-07-03 17:58:52 +0300

    Added commands to get a schedule

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import telebot
 from db import init_db, get_user_group, edit_user_group, add_new_user, check_exists_user
 from config import token
 from parser import get_schedule, print_schedule, get_group_id
-from date import get_date
+from date import get_date, get_week, check_date, reverse_date, date_addition
 
 
 def telegram_bot():
@@ -16,6 +16,7 @@ def telegram_bot():
     def send_schedule(message):
         text = message.text.split()
         telegram_id = message.from_user.id
+        schedule_commands = ['сегодня', 'завтра', 'неделя', 'следующая', 'дата']
 
         if 'группа' in text[0]:
             group_id = get_group_id(text[1])
@@ -31,24 +32,37 @@ def telegram_bot():
                 bot.send_message(message.chat.id, 'Что-то пошло не так')
             return
 
-        if 'сегодня' or 'завтра' or 'неделя' or 'следующая' or 'дата' in text[0]:
+        if any(s in text[0] for s in schedule_commands):
             if check_exists_user(telegram_id=telegram_id):
                 group_id = get_user_group(telegram_id=telegram_id)
             else:
                 bot.send_message(message.chat.id, 'Сначала нужно ввести название группы')
                 return
 
-        if 'сегодня' or 'завтра' in text[0]:
+        if 'сегодня' in text[0] or 'завтра' in text[0]:
             diff = 0
             if 'завтра' in text[0]:
                 diff = 1
             date = get_date(diff)
             schedule = get_schedule(group_id, date, date)
             bot.send_message(message.chat.id, print_schedule(schedule))
-        elif 'неделя' in text[0]:
-            pass
-        elif 'следующая' in text[0]:
-            pass
+        elif 'неделя' in text[0] or 'следующая' in text[0]:
+            next_week = False
+            if 'следующая' in text[0]:
+                next_week = True
+            start_week, finish_week = get_week(next_week)
+            schedule = get_schedule(group_id, start_week, finish_week)
+            bot.send_message(message.chat.id, print_schedule(schedule))
+        elif 'дата' in text[0]:
+            date = text[1]
+            if len(date.split('.')) == 2 or len(date.split('.')[-1]) == 0:
+                date = date_addition(date)
+            if check_date(date):
+                date = reverse_date(date)
+                schedule = get_schedule(group_id, date, date)
+                bot.send_message(message.chat.id, print_schedule(schedule))
+            else:
+                bot.send_message(message.chat.id, "Некорректная дата")
 
     bot.polling()
 
--- date.py
+++ date.py
@@ -7,3 +7,37 @@ def get_date(date_change=0):
     return date
 
 
+def get_week(next_week=False):
+    if not next_week:
+        start_week = get_date(-1 * datetime.date.weekday(datetime.date.today()))
+        finish_week = get_date(6 - datetime.date.weekday(datetime.date.today()))
+    else:
+        start_week = get_date(7 - datetime.date.weekday(datetime.date.today()))
+        finish_week = get_date(13 - datetime.date.weekday(datetime.date.today()))
+    return start_week, finish_week
+
+
+def check_date(str_date):
+    date_format = "%d.%m.%Y"
+    try:
+        datetime.datetime.strptime(str_date, date_format)
+        return True
+    except ValueError:
+        return False
+
+
+def reverse_date(date):
+    old_format = "%d.%m.%Y"
+    new_format = "%Y.%m.%d"
+    new_date = datetime.datetime.strptime(date, old_format)
+    new_date = new_date.strftime(new_format)
+    return new_date
+
+
+def date_addition(date):
+    year = datetime.date.today().timetuple()[0]
+    if date[-1] != '.':
+        date += '.'
+    new_date = date + str(year)
+    return new_date
+
